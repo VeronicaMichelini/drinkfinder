d4712760f7468f03bb219ca116ea3894e99d10c4
VeronicaMichelini <48562858+VeronicaMichelini@users.noreply.github.com>
2019-06-29 10:31:25 +0200

    Add login

---
+++ login/reg.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<body background = 'https://static.vecteezy.com/system/resources/previews/000/401/351/non_2x/vector-background-wallpaper-with-polygons-in-gradient-colors.jpg';
+
+<head>
+ <title>Nuova registrazione</title>
+</head>
+
+<body>
+<h2>Nuova registrazione</h2>
+<form method="post" action="connect.php">
+	Username : <br><input type="text" name="username"><br><br>
+	Password : <br><input type="password" name="password"><br><br>
+	<input type="submit" value="Registrati">
+</form>
+
+</body>
+
+</html>
